e44eb56eafaf2a143de16c63b5be93207ce6f54e
xLnyCoding <lennart.paetzold@gmx.de>
2024-05-15 21:29:51 +0200

    Added config load mechanism

--- src/main/java/de/kidinthedark/bedwarsplugin/BedwarsPlugin.java
+++ src/main/java/de/kidinthedark/bedwarsplugin/BedwarsPlugin.java
@@ -1,13 +1,23 @@
 package de.kidinthedark.bedwarsplugin;
 
+import de.kidinthedark.bedwarsplugin.util.ConfigLoader;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BedwarsPlugin extends JavaPlugin {
 
+    public static BedwarsPlugin instance;
+
+    public ConfigLoader configLoader;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        instance = this;
+        getLogger().info("Loading plugin...");
+        getLogger().info("Loading config files...");
+        configLoader = new ConfigLoader();
+        configLoader.loadConfig();
+        getLogger().info("Config loaded!");
     }
 
     @Override
