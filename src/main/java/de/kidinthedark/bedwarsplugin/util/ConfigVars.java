98875b3e23af8ec156d4385c555db3dfa2ffad81
xLnyCoding <lennart.paetzold@gmx.de>
2024-05-15 21:52:45 +0200

    Added value loading mechanism

---
+++ src/main/java/de/kidinthedark/bedwarsplugin/util/ConfigVars.java
@@ -0,0 +1,9 @@
+package de.kidinthedark.bedwarsplugin.util;
+
+public class ConfigVars {
+
+    public static int lobbycountdown;
+    public static int pregamecountdown;
+    public static int postgamecountdown;
+
+}
