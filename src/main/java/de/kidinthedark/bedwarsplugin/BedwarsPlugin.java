4b6b0e68d3b76161ec9703e3acabe20a008dabf9
xLnyCoding <lennart.paetzold@gmx.de>
2024-05-15 18:22:54 +0200

    Initial Upload

---
+++ src/main/java/de/kidinthedark/bedwarsplugin/BedwarsPlugin.java
@@ -0,0 +1,17 @@
+package de.kidinthedark.bedwarsplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BedwarsPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
