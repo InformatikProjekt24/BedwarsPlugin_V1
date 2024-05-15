b546b7f3922768ccdbc876a005224ef500810ace
xLnyCoding <lennart.paetzold@gmx.de>
2024-05-15 19:05:57 +0200

    Added essential GameManager files

---
+++ src/main/java/de/kidinthedark/bedwarsplugin/game/GameManager.java
@@ -0,0 +1,4 @@
+package de.kidinthedark.bedwarsplugin.game;
+
+public class GameManager {
+}
---
+++ src/main/java/de/kidinthedark/bedwarsplugin/game/GameState.java
@@ -0,0 +1,13 @@
+package de.kidinthedark.bedwarsplugin.game;
+
+public enum GameState {
+
+    PRELOBBY,
+    LOBBY,
+    PREGAME,
+    INGAME,
+    POSTGAME,
+    ENDLOBBY,
+    POSTLOBBY
+
+}
