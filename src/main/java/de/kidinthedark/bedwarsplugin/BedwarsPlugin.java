7a9a2415a5a3e4f325c490baf20d58f39fccc16d
xLnyCoding <lennart.paetzold@gmx.de>
2024-05-15 21:52:19 +0200

    Minor changes

--- src/main/java/de/kidinthedark/bedwarsplugin/BedwarsPlugin.java
+++ src/main/java/de/kidinthedark/bedwarsplugin/BedwarsPlugin.java
@@ -1,5 +1,7 @@
 package de.kidinthedark.bedwarsplugin;
 
+import de.kidinthedark.bedwarsplugin.game.GameManager;
+import de.kidinthedark.bedwarsplugin.game.UniversalSecondsGameTimer;
 import de.kidinthedark.bedwarsplugin.util.ConfigLoader;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -9,6 +11,7 @@ public final class BedwarsPlugin extends JavaPlugin {
     public static BedwarsPlugin instance;
 
     public ConfigLoader configLoader;
+    public GameManager gameManager;
 
     @Override
     public void onEnable() {
@@ -18,10 +21,14 @@ public final class BedwarsPlugin extends JavaPlugin {
         configLoader = new ConfigLoader();
         configLoader.loadConfig();
         getLogger().info("Config loaded!");
+
+
+        UniversalSecondsGameTimer.startTimer();
     }
 
     @Override
     public void onDisable() {
+        UniversalSecondsGameTimer.stopTimer();
         // Plugin shutdown logic
     }
 }
