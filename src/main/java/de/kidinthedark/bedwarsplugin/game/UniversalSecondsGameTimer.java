30e52e65ee6c9e369a40fd7620432fd105ab482b
xLnyCoding <lennart.paetzold@gmx.de>
2024-05-16 18:46:40 +0200

    Changed the game timer to synchronous for threading compability

--- src/main/java/de/kidinthedark/bedwarsplugin/game/UniversalSecondsGameTimer.java
+++ src/main/java/de/kidinthedark/bedwarsplugin/game/UniversalSecondsGameTimer.java
@@ -1,28 +1,17 @@
 package de.kidinthedark.bedwarsplugin.game;
 
 import de.kidinthedark.bedwarsplugin.BedwarsPlugin;
-import io.papermc.paper.threadedregions.scheduler.ScheduledTask;
-import org.bukkit.block.Bed;
-
-import java.util.concurrent.TimeUnit;
-import java.util.function.Consumer;
 
 public class UniversalSecondsGameTimer {
 
-    public volatile static ScheduledTask task;
+    public static int task;
 
     public static void startTimer() {
-        BedwarsPlugin.instance.getServer().getAsyncScheduler().runAtFixedRate(BedwarsPlugin.instance, new Consumer<ScheduledTask>() {
-                    @Override
-                    public void accept(ScheduledTask scheduledTask) {
-                        task = scheduledTask;
-                        BedwarsPlugin.instance.gameManager.tick();
-                    }
-                }, 1, 1, TimeUnit.SECONDS);
+        task = BedwarsPlugin.instance.getServer().getScheduler().scheduleSyncRepeatingTask(BedwarsPlugin.instance, () -> BedwarsPlugin.instance.gameManager.tick(), 20, 20);
     }
 
     public static void stopTimer() {
-        task.cancel();
+        BedwarsPlugin.instance.getServer().getScheduler().cancelTask(task);
     }
 
 }
