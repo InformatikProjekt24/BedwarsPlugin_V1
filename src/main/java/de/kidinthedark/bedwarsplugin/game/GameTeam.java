c57680ae17f176e60ab291f4bd47757526a7362d
xLnyCoding <lennart.paetzold@gmx.de>
2024-05-17 11:16:32 +0200

    Added GameTeam object for game management

---
+++ src/main/java/de/kidinthedark/bedwarsplugin/game/GameTeam.java
@@ -0,0 +1,14 @@
+package de.kidinthedark.bedwarsplugin.game;
+
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameTeam {
+
+    public boolean hasBed;
+    public boolean isAlive;
+    public List<Player> members = new ArrayList<>();
+
+}
