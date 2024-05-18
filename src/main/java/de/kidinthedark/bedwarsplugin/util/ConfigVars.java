3080f9827f9b0b0dba94e958d675952f6115b974
xLnyCoding <lennart.paetzold@gmx.de>
2024-05-18 16:07:03 +0200

    Added fields for multilingual support

--- src/main/java/de/kidinthedark/bedwarsplugin/util/ConfigVars.java
+++ src/main/java/de/kidinthedark/bedwarsplugin/util/ConfigVars.java
@@ -1,6 +1,13 @@
 package de.kidinthedark.bedwarsplugin.util;
 
+import java.util.List;
+
 public class ConfigVars {
+
+    /* Utility for multilingual support */
+    public static List<String> availableLanguages;
+    public static List<String> languageMessages;
+    public static String defaultLanguage;
 
     public static int lobbycountdown;
     public static int pregamecountdown;
