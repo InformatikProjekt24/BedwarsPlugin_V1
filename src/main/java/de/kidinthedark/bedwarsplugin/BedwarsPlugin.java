048fc65e7b737fa8b5075593a5a562b30e071757
xLnyCoding <lennart.paetzold@gmx.de>
2024-05-18 17:32:02 +0200

    Added MySQL Cleaned up

--- src/main/java/de/kidinthedark/bedwarsplugin/BedwarsPlugin.java
+++ src/main/java/de/kidinthedark/bedwarsplugin/BedwarsPlugin.java
@@ -3,13 +3,17 @@ package de.kidinthedark.bedwarsplugin;
 import de.kidinthedark.bedwarsplugin.game.GameManager;
 import de.kidinthedark.bedwarsplugin.game.UniversalSecondsGameTimer;
 import de.kidinthedark.bedwarsplugin.util.ConfigLoader;
+import de.kidinthedark.bedwarsplugin.util.ConfigVars;
 import de.kidinthedark.bedwarsplugin.util.LanguageLoader;
+import de.kidinthedark.bedwarsplugin.util.MySQL;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class BedwarsPlugin extends JavaPlugin {
 
     public static BedwarsPlugin instance;
+
+    public MySQL mySQL;
 
     public ConfigLoader configLoader;
     public GameManager gameManager;
@@ -19,6 +23,7 @@ public final class BedwarsPlugin extends JavaPlugin {
     public void onEnable() {
         instance = this;
         getLogger().info("Loading plugin...");
+
         getLogger().info("Loading config files...");
         configLoader = new ConfigLoader();
         configLoader.loadConfig();
@@ -32,6 +37,11 @@ public final class BedwarsPlugin extends JavaPlugin {
         gameManager.prepareServer();
         getLogger().info("Gamemanager loaded!");
 
+        getLogger().info("Connecting mysql...");
+        mySQL = new MySQL(ConfigVars.mysqlHost, ConfigVars.mysqlPort + "", ConfigVars.mysqlDatabase, ConfigVars.mysqlUser, ConfigVars.mysqlPassword, ConfigVars.useSSL, ConfigVars.autoReconnect, ConfigVars.allowPublicKeyRetrieval);
+        mySQL.connect();
+        createDefaultMysqlTables();
+
         UniversalSecondsGameTimer.startTimer();
     }
 
@@ -39,5 +49,9 @@ public final class BedwarsPlugin extends JavaPlugin {
     public void onDisable() {
         UniversalSecondsGameTimer.stopTimer();
         // Plugin shutdown logic
+    }
+
+    public void createDefaultMysqlTables() {
+        mySQL.update("CREATE TABLE IF NOT EXISTS playerLanguages (uuid VARCHAR(100), lang VARCHAR(10));");
     }
 }
