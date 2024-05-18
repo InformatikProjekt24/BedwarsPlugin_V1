661534af9d8b15a46f87f1d0841a28ae6c5ca8a0
xLnyCoding <lennart.paetzold@gmx.de>
2024-05-18 19:49:06 +0200

    Added logging

--- src/main/java/de/kidinthedark/bedwarsplugin/game/UniversalSecondsGameTimer.java
+++ src/main/java/de/kidinthedark/bedwarsplugin/game/UniversalSecondsGameTimer.java
@@ -7,10 +7,12 @@ public class UniversalSecondsGameTimer {
     public static int task;
 
     public static void startTimer() {
+        BedwarsPlugin.instance.getLogger().info("[UniversalSecondsGameTimer] Starting timer...");
         task = BedwarsPlugin.instance.getServer().getScheduler().scheduleSyncRepeatingTask(BedwarsPlugin.instance, () -> BedwarsPlugin.instance.gameManager.tick(), 20, 20);
     }
 
     public static void stopTimer() {
+        BedwarsPlugin.instance.getLogger().info("[UniversalSecondsGameTimer] Stopping timer...");
         BedwarsPlugin.instance.getServer().getScheduler().cancelTask(task);
     }
 
