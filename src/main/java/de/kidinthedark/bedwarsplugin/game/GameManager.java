54a100192f91edf2ae34c893cfd4c5b33bc32596
xLnyCoding <lennart.paetzold@gmx.de>
2024-05-19 16:16:18 +0200

    Added game countdown mechanism

--- src/main/java/de/kidinthedark/bedwarsplugin/game/GameManager.java
+++ src/main/java/de/kidinthedark/bedwarsplugin/game/GameManager.java
@@ -1,43 +1,64 @@
 package de.kidinthedark.bedwarsplugin.game;
 
+import de.kidinthedark.bedwarsplugin.BedwarsPlugin;
 import de.kidinthedark.bedwarsplugin.util.ConfigVars;
+import de.kidinthedark.bedwarsplugin.util.LanguagePlaceholder;
+import de.kidinthedark.bedwarsplugin.util.MessageFactory;
+import org.bukkit.Bukkit;
 
 import java.util.HashMap;
 
 public class GameManager {
 
-    private final GameState gameState = GameState.PRELOBBY;
+    private GameState gameState = GameState.LOBBY;
     private boolean busy = false;
 
     private int lobbycountdown = ConfigVars.lobbycountdown;
     private final int pregamecountdown = ConfigVars.pregamecountdown;
     private final int postgamecountdown = ConfigVars.postgamecountdown;
-
-    private final boolean ready_lobby = false;
+    private boolean lobby_wait = false;
 
     private final HashMap<String, Boolean> teamBeds = new HashMap<>();
 
     public void tick() {
         if(busy) return;
 
+        if(BedwarsPlugin.instance.mapManager.isReady() && gameState == GameState.PRELOBBY) {
+            gameState = GameState.LOBBY;
+        }
+
         if(gameState.equals(GameState.LOBBY)) {
-            boolean lobby_wait = true;
-            if(!lobby_wait) {
+            if((!lobby_wait || Bukkit.getOnlinePlayers().size() >= ConfigVars.playersRequired) && lobbycountdown != 0) {
                 lobbycountdown--;
+                LanguagePlaceholder placeholder = new LanguagePlaceholder();
+                placeholder.updatePlaceholder("time", String.valueOf(lobbycountdown));
+                placeholder.updatePlaceholder("timeUnit", (lobbycountdown==1) ? "util_unit_second" : "util_unit_seconds");
+                MessageFactory.broadcastMessage("lobby_info_countdown", placeholder);
             }
             if(lobbycountdown == 0) {
                 doPregameTasks();
             }
         }
+
+        if(gameState.equals(GameState.PREGAME)) {
+
+        }
+    }
+
+    public void startLobbyCountdown() {
+        if(gameState.equals(GameState.LOBBY)) {
+            lobby_wait = false;
+        }
     }
 
     public void prepareServer() {
         busy = true;
+        busy = false;
     }
 
     public void doPregameTasks() {
-
         busy = true;
+        gameState = GameState.PREGAME;
 
     }
 
@@ -45,5 +66,9 @@ public class GameManager {
     //GETTERS
     public GameState getGameState() {
         return gameState;
+    }
+
+    public boolean allowJoin() {
+        return gameState.equals(GameState.LOBBY);
     }
 }
