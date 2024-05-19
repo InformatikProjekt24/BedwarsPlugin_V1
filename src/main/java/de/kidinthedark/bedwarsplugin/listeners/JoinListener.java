c8056e60e732e51fba3bb2e4ce065055c1f05cfe
xLnyCoding <lennart.paetzold@gmx.de>
2024-05-19 19:49:30 +0200

    Added player lobby tp on join

--- src/main/java/de/kidinthedark/bedwarsplugin/listeners/JoinListener.java
+++ src/main/java/de/kidinthedark/bedwarsplugin/listeners/JoinListener.java
@@ -1,16 +1,20 @@
 package de.kidinthedark.bedwarsplugin.listeners;
 
+import de.kidinthedark.bedwarsplugin.util.ConfigVars;
 import de.kidinthedark.bedwarsplugin.util.LanguagePlaceholder;
 import de.kidinthedark.bedwarsplugin.util.MessageFactory;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class JoinListener implements Listener {
 
+    @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         event.joinMessage(null);
         LanguagePlaceholder placeholder = new LanguagePlaceholder();
         MessageFactory.sendMessage("lobby_info_join", placeholder, event.getPlayer());
+        event.getPlayer().teleport(ConfigVars.lobbySpawnLocation);
     }
 
 }
