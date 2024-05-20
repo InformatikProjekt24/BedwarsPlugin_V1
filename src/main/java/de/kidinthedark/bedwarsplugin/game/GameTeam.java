e063c92c481186a917917203dd2bc45a1369901d
xLnyCoding <lennart.paetzold@gmx.de>
2024-05-20 15:09:37 +0200

    Implemented fields for GameTeam

--- src/main/java/de/kidinthedark/bedwarsplugin/game/GameTeam.java
+++ src/main/java/de/kidinthedark/bedwarsplugin/game/GameTeam.java
@@ -1,5 +1,7 @@
 package de.kidinthedark.bedwarsplugin.game;
 
+import org.bukkit.Color;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
@@ -7,8 +9,23 @@ import java.util.List;
 
 public class GameTeam {
 
-    public boolean hasBed;
-    public boolean isAlive;
-    public List<Player> members = new ArrayList<>();
+    private boolean hasBed;
+    private boolean isAlive;
+    private List<Player> members;
+
+    private Color color;
+    private Location spawn;
+    private Location bed1;
+    private Location bed2;
+
+    public GameTeam(Location spawn, Location bed1, Location bed2, Color color) {
+        this.hasBed = true;
+        this.isAlive = true;
+        this.spawn = spawn;
+        this.bed1 = bed1;
+        this.bed2 = bed2;
+        this.color = color;
+        members = new ArrayList<>();
+    }
 
 }
