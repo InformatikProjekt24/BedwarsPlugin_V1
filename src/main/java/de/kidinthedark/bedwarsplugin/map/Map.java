9d350bcd42fd2d41ea7cb942077f506481b65208
xLnyCoding <lennart.paetzold@gmx.de>
2024-05-20 21:06:56 +0200

    Implemented displayName

--- src/main/java/de/kidinthedark/bedwarsplugin/map/Map.java
+++ src/main/java/de/kidinthedark/bedwarsplugin/map/Map.java
@@ -10,15 +10,21 @@ public class Map {
     private final ArrayList<GameTeam> teams;
     private final ArrayList<Generator> generators;
     private final String name;
+    private final String displayName;
 
-    public Map(String name, ArrayList<GameTeam> teams, ArrayList<Generator> generators) {
+    public Map(String name, String displayName, ArrayList<GameTeam> teams, ArrayList<Generator> generators) {
         this.name = name;
+        this.displayName = displayName;
         this.teams = teams;
         this.generators = generators;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getDisplayName() {
+        return displayName;
     }
 
     public ArrayList<GameTeam> getTeams() {
