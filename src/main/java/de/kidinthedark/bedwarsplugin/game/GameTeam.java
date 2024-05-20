04a92c17a7bb30f9a60471db85b0780f8a77c1f0
xLnyCoding <lennart.paetzold@gmx.de>
2024-05-20 21:05:51 +0200

    Implemented GameTeam

--- src/main/java/de/kidinthedark/bedwarsplugin/game/GameTeam.java
+++ src/main/java/de/kidinthedark/bedwarsplugin/game/GameTeam.java
@@ -1,31 +1,79 @@
 package de.kidinthedark.bedwarsplugin.game;
 
+import de.kidinthedark.bedwarsplugin.util.LanguagePlaceholder;
+import de.kidinthedark.bedwarsplugin.util.MessageFactory;
+import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.Location;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class GameTeam {
 
     private boolean hasBed;
-    private boolean isAlive;
-    private List<Player> members;
+    private final boolean isAlive;
 
-    private Color color;
-    private Location spawn;
-    private Location bed1;
-    private Location bed2;
+    private final Color color;
+    private final Location spawn;
+    private final Location bed1;
+    private final Location bed2;
 
-    public GameTeam(Location spawn, Location bed1, Location bed2, Color color) {
+    private final ArrayList<Player> members;
+
+    public GameTeam(Location spawn, Location bed1, Location bed2, Color color, ArrayList<Player> members) {
         this.hasBed = true;
         this.isAlive = true;
         this.spawn = spawn;
         this.bed1 = bed1;
         this.bed2 = bed2;
         this.color = color;
-        members = new ArrayList<>();
+        this.members = members;
+    }
+
+    public Location getSpawn() {
+        return spawn;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public boolean hasBed() {
+        return hasBed;
+    }
+
+    public boolean isBedLocation(Location location) {
+        return location.equals(bed1) || location.equals(bed2);
+    }
+
+    public void breakBed(Player p) {
+
+        LanguagePlaceholder placer = new LanguagePlaceholder();
+        placer.updatePlaceholder("team", "color_" + color.toString());
+
+        if(members.contains(p)) {
+            MessageFactory.sendMessage("game_action_bed_break_own", placer, p);
+            return;
+        }
+
+        MessageFactory.sendMessage("game_action_bed_break", placer, p);
+
+        this.hasBed = false;
+
+        for(Player player : Bukkit.getOnlinePlayers()) {
+            player.playSound(player.getLocation(), Sound.ENTITY_WITHER_DEATH, 1, 1);
+            if(members.contains(player)) {
+                MessageFactory.sendMessage("game_action_bed_broke_own", placer, player);
+            } else {
+                MessageFactory.sendMessage("game_action_bed_broke", placer, player);
+            }
+        }
+    }
+
+    public boolean hasMember(Player p) {
+        return members.contains(p);
     }
 
 }
