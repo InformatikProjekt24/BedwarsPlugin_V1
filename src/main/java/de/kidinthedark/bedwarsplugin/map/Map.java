79092dc37590318e9bea2ca054369839366ef5f2
xLnyCoding <lennart.paetzold@gmx.de>
2024-05-20 18:49:11 +0200

    Added fields, getters and setters - teams: ArrayList<GameTeam> - generators: ArrayList<Generator> c Map(teams: ArrayList<GameTeam>, generators: ArrayList<Generator>) + getTeams(): ArrayList<GameTeam> + getGenerators(): ArrayList<Generator>

--- src/main/java/de/kidinthedark/bedwarsplugin/map/Map.java
+++ src/main/java/de/kidinthedark/bedwarsplugin/map/Map.java
@@ -1,4 +1,26 @@
 package de.kidinthedark.bedwarsplugin.map;
 
+import de.kidinthedark.bedwarsplugin.game.GameTeam;
+import de.kidinthedark.bedwarsplugin.map.generators.Generator;
+
+import java.util.ArrayList;
+
 public class Map {
+
+    private ArrayList<GameTeam> teams;
+    private ArrayList<Generator> generators;
+
+    public Map(ArrayList<GameTeam> teams, ArrayList<Generator> generators) {
+        this.teams = teams;
+        this.generators = generators;
+    }
+
+    public ArrayList<GameTeam> getTeams() {
+        return teams;
+    }
+
+    public ArrayList<Generator> getGenerators() {
+        return generators;
+    }
+
 }
