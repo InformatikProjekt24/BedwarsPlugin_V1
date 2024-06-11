63a15fb0bc3d84edae2072d0a1cfb8edf5857c94
HuskiyRaven <timofeyrotov@gmail.com>
2024-06-11 08:25:35 +0200

    Update UpgradeShopCategory.java
    Started upgrade shop

--- src/main/java/de/kidinthedark/bedwarsplugin/game/UpgradeShopCategory.java
+++ src/main/java/de/kidinthedark/bedwarsplugin/game/UpgradeShopCategory.java
@@ -1,7 +1,33 @@
 public enum UpgradeShopCategory {
-  FORGE,
-  HASTE,
-  SHARPNESS,
-  PROTECTION,
-  TRAP
+  FORGE("Forge", Material.OVEN, 10),
+  HASTE("Haste", Material.GOLD_PICKAXE, 11),
+  SHARPNESS("Sharpness", Material.IRON_SWORD, 12),
+  PROTECTION("Protection", Material.IRON_CHESTPLATE, 13),
+  TRAP("Trap", Material.TRIPWIRE_HOOK, 14);
+
+  final String name;
+  final Material m;
+  final int slot;
+
+  private UpgradeShopCategory(String name, Material m, int slot) {
+    this.name = name;
+    this.m = m;
+    this. slot = slot;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public Material getMaterial() {
+    return m;
+  }
+
+  public int getSlot() {
+    return slot;
+  }
+
+  public String getLore() {
+    return "damn this gonna be annoying";
+  }
 }
